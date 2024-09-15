c8aee8b7fd0f1e0fdea7251bd37111e50249e5ea
fisunoff <fisunoff@yandex.ru>
2024-09-15 14:54:23 +0300

    Лабораторная 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 
 #include <iostream>
+#include <vector>
 
 #include <GL/gl.h>
 #include <GL/glu.h>
@@ -8,6 +9,19 @@
 
 // используем пространство имен стандартной библиотеки
 using namespace std;
+
+#define CHANGE_TIME_MS 2000
+
+int index = 0;
+vector<double> black = { 0, 0, 0 };
+vector<double> white = { 1, 1, 1 };
+vector<double> blue = { 0, 0, 1 };
+vector<double> red = { 1, 0, 0 };
+vector<double> violet = { 1, 0, 1 };
+
+vector< vector<double>> colors = { black, white, blue, red, violet };
+int max_index = colors.size();
+
 
 // функция, вызываемая при изменении размеров окна
 void reshape(int w, int h)
@@ -26,7 +40,7 @@ void reshape(int w, int h)
 void display(void)
 {
 	// отчищаем буфер цвета и буфер глубины
-	glClearColor(0.22, 0.88, 0.11, 1.0);
+	glClearColor(0.2, 0.88, 0.11, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// включаем тест глубины
@@ -38,7 +52,8 @@ void display(void)
 	gluLookAt(5, 5, 7.5, 0, 0, 0, 0, 1, 0);
 
 	// выводим объект - красный (1,0,0) чайник
-	glColor3f(1.0, 0.0, 0.0);
+	auto color_now = colors[index];
+	glColor3f(color_now[0], color_now[1], color_now[2]);
 	glutWireTeapot(1.0);
 
 	// смена переднего и заднего буферов
@@ -54,10 +69,22 @@ void simulation(int value)
 	glutTimerFunc(20, simulation, 0);
 };
 
+
+#ifdef CHANGE_TIME_MS
+void change_color_by_timer(int value) {
+	// смена цвета по таймеру каждые CHANGE_TIME_MS мс
+	index = (index + 1) % max_index;
+	glutPostRedisplay();
+	// эта же функция будет вызвана еще раз через  мс
+	glutTimerFunc(CHANGE_TIME_MS, change_color_by_timer, 0);
+}
+#endif
+
 // Функция обработки нажатия клавиш
 void keyboardFunc(unsigned char key, int x, int y)
 {
-	printf("Key code is %i\n", key);
+	index = (index + 1) % max_index;
+	printf("Key code is %i\n Color changed\n", key);
 };
 
 void main(int argc, char** argv)
@@ -86,6 +113,10 @@ void main(int argc, char** argv)
 	glutTimerFunc(20, simulation, 0);
 	// устанавливаем функцию, которая будет вызываться при нажатии на клавишу
 	glutKeyboardFunc(keyboardFunc);
+	
+	#ifdef CHANGE_TIME_MS
+	glutTimerFunc(20, change_color_by_timer, 0); // смена цвета по таймеру каждые CHANGE_TIME_MS мс
+	#endif
 
 	// основной цикл обработки сообщений ОС
 	glutMainLoop();
